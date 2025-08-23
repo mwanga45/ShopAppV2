1e94ff39ba4ec06649eed44faa500a64d39aa02b
Your Name <you@example.com>
2025-08-23 10:28:52 +0300

    more changes

--- src/component/Sidebar.tsx
+++ src/component/Sidebar.tsx
@@ -1,17 +1,28 @@
 import "./sidebar.css"
+import { SiShopware } from "react-icons/si";
 export const Sidebar = ()=>{
   return(
     <div className="nav-container">
     <div className="logo-container">
-      <h2>Mwanga-ShoappV2 </h2>
+      <h2>ShoappV2 <SiShopware size={30}color="green"/> </h2>
     </div>
     <div className="navlist-container">
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+       <li className="nav-name">
+        <p>Dashboard</p> 
+       </li>
+       <li className="nav-name">
+        <p>Stock</p>
+       </li>
+       <li className="nav-name">
+        <p>Whole-sales</p>
+       </li>
+       <li className="nav-name">
+        <p>Retails-sales</p>
+       </li>
+       <li className="nav-name">
+        <p>Setting</p>
+       </li>
       </ul>
     </div>
     </div>
