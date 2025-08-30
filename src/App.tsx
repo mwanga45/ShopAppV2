50f85cab4f9e3c8e26917811b00896767a6ba677
mwanga45 <issamwanga02@gmail.com>
2025-08-30 11:25:38 +0300

    more changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Login from './auth/login'
 import Layout from './Layout'
 import './App.css'
 import { Dashboard } from './Dashbord/dashbord'
-
+import SalesToday from "./Sales/SalesToday"
 function App() {
   return (
    <div>
@@ -11,6 +11,7 @@ function App() {
           <Route path='/' element = {<Login/>} />
            <Route element={<Layout/>}>
             <Route path='/dashboard'element= {<Dashboard/>}></Route>
+            <Route path='/sales' element ={<SalesToday/>}></Route>
             </Route>
         </Routes>
    </div> 
---
+++ src/Sales/SalesToday.tsx
@@ -0,0 +1,15 @@
+import "./sales.css"
+
+export default function SalesToday() {
+  return (
+    <div className="sales-container">
+       <div className="salesbar">
+         
+       </div>
+       <div className="sales-title">
+        <p className="title">Product sales Today</p>
+       </div>
+       div
+    </div>
+  )
+}
