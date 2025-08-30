afbd218c3ab430b899451959a372ab073c4f370d
mwanga45 <issamwanga02@gmail.com>
2025-08-30 12:22:42 +0300

    more changes

--- src/Sales/SalesToday.tsx
+++ src/Sales/SalesToday.tsx
@@ -1,15 +1,29 @@
 import "./sales.css"
+import { Accountbar } from "../component/account/Account"
+import { Search } from "../component/search/Search"
 
 export default function SalesToday() {
   return (
-    <div className="sales-container">
+    <div className="Wh-Rtsales-container">
        <div className="salesbar">
-         
+           <Accountbar/>
        </div>
        <div className="sales-title">
         <p className="title">Product sales Today</p>
        </div>
-       div
+       <div className="trigger-container">
+        <div className="trigger-btn">
+            <button type="submit" name="Whole-sales">Whole-sales</button>
+            <button type="submit" name="Retail-ssales">Retail-sales</button>
+        </div>
+        <div>
+           <Search/>
+        </div>
+       </div>
+       <div className="whole-retail-analysis">
+        
+       </div>
+
     </div>
   )
 }
--- src/component/daysales/Daysales.tsx
+++ src/component/daysales/Daysales.tsx
@@ -1,12 +1,29 @@
 import "./daysales.css"
 import { SiMoneygram } from "react-icons/si";
+
+
+type Result ={
+  title_name:string
+  total_value:string
+}
 export const Daysales = ()=>{
     return(
       <div className="sales-container">
         <p className="head">Today Sales</p>
         <p className="Amount"> <SiMoneygram color="black" size={20}/>2300000 Tsh</p>
       </div>
-    )
-        
-    
+    ) 
+}
+export const DayResult = ({title_name, total_value}:Result)=>{
+  return(
+    <div className="dayresult-contaier">
+      <div>
+        <p className="dayanalys">{title_name}</p>
+      </div>
+      <div>
+        <p className="total_value">{total_value}</p>
+      </div>
+
+    </div>
+  )
 }
