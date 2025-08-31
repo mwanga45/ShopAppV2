83e95c2eb085ab0322bd8930ce68d0b74ccf5755
mwanga45 <issamwanga02@gmail.com>
2025-08-31 12:54:11 +0300

    stock init

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Layout from './Layout'
 import './App.css'
 import { Dashboard } from './Dashbord/dashbord'
 import SalesToday from "./Sales/SalesToday"
+import Stock from './stock/Stock'
 function App() {
   return (
    <div>
@@ -12,6 +13,7 @@ function App() {
            <Route element={<Layout/>}>
             <Route path='/dashboard'element= {<Dashboard/>}></Route>
             <Route path='/sales' element ={<SalesToday/>}></Route>
+            <Route path='/stock' element = {<Stock/>}></Route>
             </Route>
         </Routes>
    </div> 
--- src/stock/Stock.tsx
+++ src/stock/Stock.tsx
@@ -1,8 +1,14 @@
-
+import { Accountbar } from "../component/account/Account"
+import "./stock.css"
 export default function Stock() {
   return (
-    <div>
-      
+    <div className="stock-main-conatiner">
+           <div className="account">
+               <Accountbar/>
+            </div>   
+        <div className="sales-header">
+          <h1 className="page-title">Stock </h1>
+       </div>
     </div>
   )
 }
