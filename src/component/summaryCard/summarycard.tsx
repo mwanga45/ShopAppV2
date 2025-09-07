fe842c6f5b576705cefe253f275d14bd6a52835d
mwanga45 <issamwanga02@gmail.com>
2025-09-08 01:38:33 +0300

    more changes

--- src/component/summaryCard/summarycard.tsx
+++ src/component/summaryCard/summarycard.tsx
@@ -1,9 +1,27 @@
+import type React from "react"
 import "./summarycard.css"
+interface SummaryCardProps {
+   icon:React.ElementType
+   SummaryTitle:string,
+   SummaryActInfo:string
 
-export const SummaryCard =()=>{
+
+}
+export const SummaryCard:React.FC<SummaryCardProps> =({icon:Icon,SummaryActInfo,SummaryTitle})=>{
     return(
         <div className="summary-card-container">
-            
+            <div className="summary-icon">
+               <Icon/>
+            </div>
+            <div className="summary-info">
+                <p className="summary titlename">
+                    {SummaryTitle}
+                </p>
+                <p className="summary-act-info">
+                    {SummaryActInfo}
+                </p>
+            </div>
+
         </div>
     )
 
