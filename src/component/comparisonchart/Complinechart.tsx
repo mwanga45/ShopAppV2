b6ffa8c024dd2aca3130d4ea1f1fe026df2210db
mwanga45 <issamwanga02@gmail.com>
2025-09-08 09:50:46 +0300

    more changes

---
+++ src/component/comparisonchart/Complinechart.tsx
@@ -0,0 +1,31 @@
+import { 
+    ResponsiveContainer,
+    Tooltip,
+    Legend,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianAxis,
+    CartesianGrid
+    
+} from "recharts"
+export const Complinechart = ()=>{
+    const Thisweek = [
+        {day:"Monday", Sales:"23450"},
+        {day:"Tuesday", Sales:"20400"},
+        {day:"Wensday", Sales:"25400"},
+        {day:"Thursday", Sales:"26700"},
+        {day:"Friday", Sales:"33400"},
+        {day:"Saturday", Sales:"44400"},
+        {day:"Sunday", Sales:"23510"}
+    ]
+    const LastWeek = [
+        {day:"Monday", Sales:"20400"},
+        {day:"Tuesday", Sales:"23400"},
+        {day:"Wensday", Sales:"22400"},
+        {day:"Thurday", Sales:"19400"},
+        {day:"Friday", Sales:"25500"},
+       
+    ]
+
+}
