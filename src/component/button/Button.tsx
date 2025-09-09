528f3270a451b8b21705c0310f5137dfa1b07102
mwanga45 <issamwanga02@gmail.com>
2025-09-10 00:48:24 +0300

    more changes

--- src/component/button/Button.tsx
+++ src/component/button/Button.tsx
@@ -3,11 +3,16 @@ import "./button.css"
 
 interface ButtonTraits {
   buttonName:string
+  Onclick?:React.MouseEventHandler<HTMLButtonElement>
 }
-export const  Button:React.FC<ButtonTraits> =({buttonName})=> {
-  const handleClick = () => {
+export const  Button:React.FC<ButtonTraits> =({buttonName,Onclick})=> {
+  const handleClick:React.MouseEventHandler<HTMLButtonElement> = (e) => {
+    if(Onclick){
+      Onclick(e)
+    }
     console.log("Button clicked!")
   }
+
 
   return (
     <button className="animated-btn" onClick={handleClick}>
