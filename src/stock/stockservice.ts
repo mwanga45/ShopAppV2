4e3a38571d2751bf6c929c6995afad8b61e28c1b
mwanga45 <issamwanga02@gmail.com>
2025-09-13 00:17:46 +0300

    more changes

--- src/stock/stockservice.ts
+++ src/stock/stockservice.ts
@@ -9,3 +9,12 @@ export const StockUpdate = async(id:string,updatestock:any)=>{
       throw err
     }
 }
+export const Stockresult =async()=>{
+    try{
+        const stockinfo =     await api.get('/stock/stockresult')
+        return stockinfo
+    }catch(err){
+        console.error("failed to get Stockresult", err)
+        throw err
+    }
+}
