5d447126d39f9551bb11fe30602372f3d8d515f8
mwanga45 <issamwanga02@gmail.com>
2025-09-12 15:09:13 +0300

    more changes

--- src/component/account/Account.tsx
+++ src/component/account/Account.tsx
@@ -1,15 +1,36 @@
-import "./account.css"
+import {jwtDecode }from "jwt-decode"; 
+import "./account.css";
 import { PiUserCircleDashedFill } from "react-icons/pi";
-export const Accountbar = ()=>{
-    return(
+
+interface TokenPayload {
+  sub: string | null;
+  email: string | null;
+  role: string | null;
+  fullname:string |null
+}
+
+export const Accountbar = () => {
+  const token = localStorage.getItem("access_token");
+
+  let userInfo: TokenPayload | null = null;
+
+  if (token) {
+
+    userInfo = jwtDecode<TokenPayload>(token);
+  }
+
+  console.log(userInfo);
+
+  return (
     <div className="ac-container">
       <div className="avator">
-        <PiUserCircleDashedFill color="pink" size={40} enableBackground={"pink"}/>
+        <PiUserCircleDashedFill color="pink" size={40} />
       </div>
       <div className="acc-details">
-        <p className="acc-name">Issa mwanga</p>
-        <p className="acc-email">issamwanga02@gmail.com</p>
+        <p className="acc-name">{userInfo?.fullname?? "Guest"}</p>
+        <p className="acc-name">{userInfo?.role ?? "Invited"}</p>
+        <p className="acc-email">{userInfo?.email ?? "Not logged in"}</p>
       </div>
-    </div> 
-    )
-}
+    </div>
+  );
+};
