c0eefe963026e124d0debb22ce93dafe23e4df57
mwanga45 <issamwanga02@gmail.com>
2025-09-16 22:38:36 +0300

    more changes

---
+++ src/component/account/otherAc.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function otherAc() {
+  return (
+    <div className='Ac-list-main-container'>
+        <div className="image-container">
+
+        </div>
+        <div className="info-detailer">
+
+        </div>
+        <div className="button-view-more">
+            
+        </div>
+
+      
+    </div>
+  )
+}
