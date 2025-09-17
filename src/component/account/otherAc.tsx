cc577070039ece6d4fd2eeedbba1780f3b089c07
mwanga45 <issamwanga02@gmail.com>
2025-09-17 10:54:19 +0300

    more changes

--- src/component/account/otherAc.tsx
+++ src/component/account/otherAc.tsx
@@ -11,25 +11,25 @@ interface OtherAcType{
     isActive?:boolean
     id?:number
 } 
-export const  OtherAc:React.FC<OtherAcType> =({isActive,fullname,email,role,id})=> {
+export const  OtherAc:React.FC<OtherAcType> =({isActive,fullname,email,role})=> {
   return (
     <div className='Ac-list-main-container' >
         <div className="image-container">
             {role==="admin"?(
             isActive === true ?(
-                    <ImUserTie color="green" size={30} />
+                    <ImUserTie color="#3498db" size={30} />
                 ):(
-                    <ImUserTie color="red" size={30} />
+                    <ImUserTie color="#e74c3c" size={30} />
                 )
             ):role === "user"?(
                 isActive === true ?(
-                    <FaUserCheck color="green" size={30}/>
+                    <FaUserCheck color="#2ecc71" size={30}/>
 
                 ):(
-                   <FaUserCheck color="green" size={30}/>
+                   <FaUserCheck color="#e74c3c" size={30}/>
                 )
             ):(
-            <GrStatusUnknown color="white" size={30}/>
+            <GrStatusUnknown color="#f1c40f" size={30}/>
             )
             }
         </div>
@@ -38,7 +38,7 @@ export const  OtherAc:React.FC<OtherAcType> =({isActive,fullname,email,role,id})
           <p className="email-info">{email}</p>
         </div>
         <div className="button-view-more">
-            <  TbHandClick  color="white" size={20} />
+            <  TbHandClick  color="#3498db" size={20} />
         </div>
 
       
