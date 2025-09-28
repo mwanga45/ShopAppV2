cbe2c1384a72bc9b8b66ac364967f69b04a238c5
mwanga45 <issamwanga02@gmail.com>
2025-09-28 11:06:14 +0300

    more changes

--- src/component/admin-reg/admin-reg.tsx
+++ src/component/admin-reg/admin-reg.tsx
@@ -1,8 +1,13 @@
-
+import "./admin-reg.css"
 export const AdminReg = () => {
   return (
-    <div>
-        <p>Admin page Activity</p>
+    <div className="admin-reg-container">
+        <h2>Admin Action</h2>
+        <div className="info-form-container">
+           <div className="info-bar">
+            
+           </div>
+        </div>
     </div>
   )
 }
