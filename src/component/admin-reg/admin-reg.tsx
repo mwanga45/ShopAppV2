30925845e5bb85dc93ca32aeb6a50e5e7e5be686
mwanga45 <issamwanga02@gmail.com>
2025-10-01 12:55:18 +0300

    more changes

--- src/component/admin-reg/admin-reg.tsx
+++ src/component/admin-reg/admin-reg.tsx
@@ -4,7 +4,49 @@ import { SummaryCard } from "../summaryCard/summarycard"
 import { FaUserShield } from "react-icons/fa"
 import { MdPerson } from "react-icons/md"
 import { FormRegUser } from "./form-reg-user"
+import { Account_details } from "../../AdminPanel/adminservice"
+import { useEffect, useState } from "react"
+export interface AccResponse {
+  CreatedAt?: string;       
+  fullname?: string;
+  email?: string;
+  phone_number?: string;
+  nida?: string;
+  role?: 'admin' | 'user' | string;  
+  isActive?: boolean;
+}
+export interface RoleCountResponse {
+  role: string;           
+  total: number;          
+  activeCount: number;    
+}
+
 export const AdminReg = () => {
+    const [loading, setloading] = useState<boolean>(true)
+    const [Acc_info, setAcc_info] = useState<AccResponse[]>([])
+    const [rolecount , setrolecount] = useState<RoleCountResponse []>([])
+
+    useEffect(()=>{
+        const handleAcc_info = async()=>{
+            try{
+            const  response = await Account_details()
+             if(!response.data.success){
+                alert(response.data.message)
+                return
+             }
+             setAcc_info(response.data.data.return_user)
+             setrolecount(response.data.data.roleAndisActive)
+             
+            }catch(err){
+                console.error(err)
+                throw (err)
+            }finally{
+              setloading(false)
+            }
+        }
+        handleAcc_info()
+
+    }, [])
     return (
         <div className="admin-reg-container">
             <div className="title-bar-reg">
@@ -18,9 +60,11 @@ export const AdminReg = () => {
                 </div>
                 <div className="list-reg-verification">
                     <div className="list-reg-container">
-                        <OtherAc/>
-                        <OtherAc/>
-                        <OtherAc/>
+                        {
+                         Acc_info.map((A)=>(
+                            <OtherAc email= {A.email} isActive = {A.isActive} role={A.role}/>
+                         ))
+                        }
                     </div>
                     <div className="user-reg-form">
                         <FormRegUser/>
