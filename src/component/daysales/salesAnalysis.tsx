7059673f95db2dbc739c8ed792de5ee376cffe48
mwanga45 <issamwanga02@gmail.com>
2025-10-11 18:45:07 +0300

    more chnages

--- src/component/daysales/salesAnalysis.tsx
+++ src/component/daysales/salesAnalysis.tsx
@@ -1,8 +1,17 @@
-
-export const AdminsalesAnaysis = ()=>{
-    return(
-        <div>
-
-        </div>
-    )
-}
+export const AdminsalesAnaysis = () => {
+  return (
+    <div className="salesAnaysis-main-container">
+      <div className="salesAnalysis-title-container">
+        <span></span>
+        <span></span>
+      </div>
+      <div className="salesdayasanalysis-container">
+        <div className="sales-analysis-graph-container"></div>
+        <div className="salasAnalysis-card-container"></div>
+      </div>
+      <div className="order-debt-record-deviation">
+          
+      </div>
+    </div>
+  );
+};
