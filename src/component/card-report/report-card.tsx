9b78ab6321915599f60dd259e8257c868489e427
mwanga45 <issamwanga02@gmail.com>
2025-10-12 16:49:23 +0300

    more changes

--- src/component/card-report/report-card.tsx
+++ src/component/card-report/report-card.tsx
@@ -1,16 +1,18 @@
 import './card.css'
+import { FaArrowUp } from "react-icons/fa6";
+import { GiReceiveMoney } from "react-icons/gi";
 export const Reportcard = ()=>{
 
     return(
         <div className="report-card-container">
-            <div>
-                <span></span>
+            <div className='report-card-container-title'>
+                <span className='title-report-card'>Total Stock Out</span>
             </div>
-            <div>
-                <span></span>
+            <div className='report-show-amount'>
+               <GiReceiveMoney size={20} color='gold'/> <span className='report-main-info'>250000 Tsh</span>
             </div>
-            <div>
-                icon <span></span> <span></span>
+            <div className='report-card-info-shown'>
+                <FaArrowUp color='green' fontSize={14}/> <span style={{color:"green", fontSize:"14px", fontWeight:"bold"}}>28 %</span> <span style={{color:"green", fontSize:"14px", fontWeight:"bold"}}> still too small</span>
             </div>
         </div>
     )
