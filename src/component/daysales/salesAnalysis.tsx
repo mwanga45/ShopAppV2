59375dae0195dc2be4983c169fc5362bb9873eb9
mwanga45 <issamwanga02@gmail.com>
2025-10-12 14:12:17 +0300

    more change

--- src/component/daysales/salesAnalysis.tsx
+++ src/component/daysales/salesAnalysis.tsx
@@ -1,20 +1,31 @@
-import './daysales.css'
+import "./daysales.css";
 export const AdminsalesAnaysis = () => {
   return (
     <div className="salesAnaysis-main-container">
       <div className="salesAnalysis-title-container">
-        <span className='analysis-first-title'>Sales</span>
-        <span className='analysis-second-title'>Overview of Your sales Performance today </span>
+        <span className="analysis-first-title">Sales</span>
+        <span className="analysis-second-title">
+          Overview of Your sales Performance today{" "}
+        </span>
       </div>
       <div className="salesdayasanalysis-container">
         <div className="sales-analysis-graph-container"></div>
-        <div className="salasAnalysis-card-container"></div>
+        <div className="salasAnalysis-card-container">
+          <div className="business-card-comaparizone">
+            <div className="first-card-comparizone"></div>
+            <div className="second-card-comparizone"></div>
+          </div>
+          <div className="business-card-comaparizone">
+            <div className="first-card-comparizone"></div>
+            <div className="second-card-comparizone"></div>
+          </div>
+        </div>
       </div>
       <div className="order-debt-record-deviation">
-          <div className="today-debtor-list"></div>
-          <div className="order-list-total-deviation">
-            <div className="order-card-deviation"></div>
-          </div>
+        <div className="today-debtor-list"></div>
+        <div className="order-list-total-deviation">
+          <div className="order-card-deviation"></div>
+        </div>
       </div>
     </div>
   );
