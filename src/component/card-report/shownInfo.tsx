c45d2c75c3a2f067040ac3016843f9ab244b2b60
mwanga45 <issamwanga02@gmail.com>
2025-10-12 18:04:44 +0300

    more chnages

--- src/component/card-report/shownInfo.tsx
+++ src/component/card-report/shownInfo.tsx
@@ -6,7 +6,9 @@ export  const ShowinfoCard = ()=>{
         <div className="shownInfo-card-container">
            <div className='showninfo-main-info'><span>42</span></div>
              <div className='showinfo-description'><span>New Order <FaArrowUp/> </span></div>
-               <div className='showninfo-roll'></div>
+               <div className='showninfo-roll'>
+                <span className="iron-rod"></span>
+               </div>
         </div>
     )
 }
