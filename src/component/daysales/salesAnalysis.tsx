1e5597e1b1903e0585ea7272b03d35a8d048078f
mwanga45 <issamwanga02@gmail.com>
2025-10-12 15:02:25 +0300

    more changes

--- src/component/daysales/salesAnalysis.tsx
+++ src/component/daysales/salesAnalysis.tsx
@@ -12,18 +12,21 @@ export const AdminsalesAnaysis = () => {
         <div className="sales-analysis-graph-container"></div>
         <div className="salasAnalysis-card-container">
           <div className="business-card-comaparizone">
-            <div className="first-card-comparizone"></div>
-            <div className="second-card-comparizone"></div>
+            <div className="first-card-comparizone">stock-in</div>
+            <div className="second-card-comparizone">stock-out</div>
           </div>
           <div className="business-card-comaparizone">
-            <div className="first-card-comparizone"></div>
-            <div className="second-card-comparizone"></div>
+            <div className="first-card-comparizone">profi comparizone today</div>
+            <div className="second-card-comparizone">profit comparizone yesterday</div>
           </div>
         </div>
       </div>
       <div className="order-debt-record-deviation">
         <div className="today-debtor-list"></div>
         <div className="order-list-total-deviation">
+          <div className="title-orderlist-deviatian-container">
+            <span>Order Placement And Deviation </span>
+          </div>
           <div className="order-card-deviation"></div>
         </div>
       </div>
