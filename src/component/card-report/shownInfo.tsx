cee3954fb1d1b21d1087c53c2e6f9324cb80af2a
mwanga45 <issamwanga02@gmail.com>
2025-10-12 17:42:37 +0300

    more changes

--- src/component/card-report/shownInfo.tsx
+++ src/component/card-report/shownInfo.tsx
@@ -1,10 +1,11 @@
-import { FaUpDown } from 'react-icons/fa6'
+
+import { FaArrowUp } from "react-icons/fa6";
 import './card.css'
 export  const ShowinfoCard = ()=>{
     return(
         <div className="shownInfo-card-container">
-           <div className='showninfo-main-info'><span></span></div>
-             <div className='showinfo-description'><span>New Order <FaUpDown/> </span></div>
+           <div className='showninfo-main-info'><span>42</span></div>
+             <div className='showinfo-description'><span>New Order <FaArrowUp/> </span></div>
                <div className='showninfo-roll'></div>
         </div>
     )
