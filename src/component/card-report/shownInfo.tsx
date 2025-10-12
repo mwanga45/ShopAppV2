ecdeb767d6e23dcee1e6bf2398bc5f58044ecb80
mwanga45 <issamwanga02@gmail.com>
2025-10-12 18:25:33 +0300

    more changes

--- src/component/card-report/shownInfo.tsx
+++ src/component/card-report/shownInfo.tsx
@@ -5,7 +5,7 @@ export  const ShowinfoCard = ()=>{
     return(
         <div className="shownInfo-card-container">
            <div className='showninfo-main-info'><span>42</span></div>
-             <div className='showinfo-description'><span>New Order <FaArrowUp/> </span></div>
+             <div className='showinfo-description'><span>New Order <FaArrowUp color="green"/> 30% </span></div>
                <div className='showninfo-roll'>
                 <span className="iron-rod"></span>
                </div>
