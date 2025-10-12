b20ccb3610fc61dcde2fb564228782edaaca7ca1
mwanga45 <issamwanga02@gmail.com>
2025-10-12 15:48:59 +0300

    more changes

--- src/component/daysales/salesAnalysis.tsx
+++ src/component/daysales/salesAnalysis.tsx
@@ -1,4 +1,5 @@
 import "./daysales.css";
+import { SiWikibooks } from "react-icons/si";
 export const AdminsalesAnaysis = () => {
   return (
     <div className="salesAnaysis-main-container">
@@ -9,8 +10,8 @@ export const AdminsalesAnaysis = () => {
         </span>
       </div>
       <div className="product-sales-track-each">
-         <span>
-          
+         <span  style={{display:'flex',columnGap:'2px', cursor:"pointer", justifyContent:"center", alignItems:"center", boxShadow: "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px", width:'auto', height:"auto", padding:"10px" , borderRadius:'10px'}}>
+            <span style={{height:'40px', width:"40px", display:'flex', justifyContent:"center", alignContent:"center", backgroundColor:"blueviolet", borderRadius:"50%",boxShadow: "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px", border:"3px, solid,whitesmoke"}}><SiWikibooks color="white" fontWeight={700}/></span><span style={{color:'grey', fontWeight:"bolder", fontSize:"18px"}}>View sales Each</span>
          </span>
       </div>
       <div className="salesdayasanalysis-container">
