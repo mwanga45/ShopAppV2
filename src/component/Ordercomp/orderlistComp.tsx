8a764155e54fc9df031d22c0938cae98e9a73942
mwanga45 <issamwanga02@gmail.com>
2025-10-13 23:45:26 +0300

    more changes

--- src/component/Ordercomp/orderlistComp.tsx
+++ src/component/Ordercomp/orderlistComp.tsx
@@ -4,22 +4,26 @@ export const OrderlistComp = () => {
     <div className="Ord-list-main-container">
       <div>
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>ID</li>
+          <li>Customer name</li>
+          <li>Product name</li>
+          <li>Product Quantity</li>
+          <li>PayAmount</li>
+          <li>PaidAmount</li>
+          <li>Paid Status</li>
+          <li>Reciving Date</li>
         </ul>
       </div>
       <div>
         <ul>
-         <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+         <li>100</li>
+         <li>Jonh Doe</li>
+          <li>Tumbili seed</li>
+          <li>2 Bags</li>
+          <li>50000</li>
+          <li>250000</li>
+          <li>Partialpaid</li>
+          <li>27-08-2025</li>
         </ul>
       </div>
     </div>
