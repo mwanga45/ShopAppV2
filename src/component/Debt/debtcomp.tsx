d2c21a54d3bd127f2c59dff1f4efa146cd59915e
mwanga45 <issamwanga02@gmail.com>
2025-10-17 17:13:24 +0300

    more changes

--- src/component/Debt/debtcomp.tsx
+++ src/component/Debt/debtcomp.tsx
@@ -1,9 +1,16 @@
 import "./debtcomp.css";
-import './CardDiscript.css'
-import type { CardDiscriptionInterface, DebtResponse } from "../../type.interface";
+import "./CardDiscript.css";
+import type {
+  CardDiscriptionInterface,
+  DebtResponse,
+} from "../../type.interface";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { useState } from "react";
-export const Debtcompo:React.FC<DebtResponse> = ({findUserDebtInfo, findtrack,PersonDebt}) => {
+export const Debtcompo: React.FC<DebtResponse> = ({
+  findUserDebtInfo,
+  findtrack,
+  PersonDebt,
+}) => {
   return (
     <div>
       <div className="Dbt-compo-list-title-container">
@@ -18,64 +25,99 @@ export const Debtcompo:React.FC<DebtResponse> = ({findUserDebtInfo, findtrack,Pe
         <span>Partalpaid</span>
       </div>
       <div className="Dbt-compo-list-container">
-        {
-         PersonDebt && PersonDebt.length > 0 ?(
+        {PersonDebt && PersonDebt.length > 0 ? (
           PersonDebt.map((item) => (
-            <CardDiscription name={item.debtor_name} date={item.deadlinedate} amount={Number(item.total_revenue).toLocaleString()} title="Remmaing"/>
-
+            <CardDiscription
+              name={item.debtor_name}
+              date={item.deadlinedate}
+              amount={Number(item.total_revenue).toLocaleString()}
+              title="Remmaing"
+            />
           ))
-
-         ):(
+        ) : (
           <span>No debt available</span>
-         )
-         
-        }
-
+        )}
       </div>
     </div>
   );
 };
-export const CardDiscription:React.FC<CardDiscriptionInterface> = ({name, date, amount, title, id})=>{
-  const [openDispay, setDisplay] = useState<boolean>(true)
-     return(
-      <div  className="crd-dsc-main-cont">
-        <div className="crd-dsc-cont" key={id}>
-            <div className="crd-dsc-icon-cont" >
-               <AiOutlineLoading3Quarters size={25} color="green" fontSize={28} fontWeight={700}/>
-            </div>
-            <div className="crd-dsc-desc-info">
-                <span className="crd-dcs-name">{name}</span>
-                <span className="crd-dcs-amount"><span>{title}</span> {amount}Tsh</span>
-            </div>
-            <div className="crd-dcs-date-container">
-                <span>{String(date).split('T')[0]}</span>
-            </div>
+export const CardDiscription: React.FC<CardDiscriptionInterface> = ({
+  name,
+  date,
+  amount,
+  title,
+  id,
+}) => {
+  const [openDispay, setDisplay] = useState<boolean>(true);
+  return (
+    <div className="crd-dsc-main-cont">
+      <div className="crd-dsc-cont" key={id}>
+        <div className="crd-dsc-icon-cont">
+          <AiOutlineLoading3Quarters
+            size={25}
+            color="green"
+            fontSize={28}
+            fontWeight={700}
+          />
         </div>
-        { openDispay &&
-
+        <div className="crd-dsc-desc-info">
+          <span className="crd-dcs-name">{name}</span>
+          <span className="crd-dcs-amount">
+            <span>{title}</span> {amount}Tsh
+          </span>
+        </div>
+        <div className="crd-dcs-date-container">
+          <span>{String(date).split("T")[0]}</span>
+        </div>
+      </div>
+      {openDispay && (
         <div className="display-card-info-main-cont">
           <div className="display-card-info-cont">
-                <Displayboard/>
+            <Displayboard />
           </div>
         </div>
-        }
-        </div>
-
-     )
-}
-
-export const Displayboard =() => {
-  return(
-    <div className="Displayboard-main-container">
-         <div className="Displayboard-info-cont">
-            <div className="series-Displayboard-info-countdown">
-              <div className="series-title-container">
-                <span> Mama Kifitiri Payment Series</span>
-                <span>Product name: Pallet starter</span>
-              </div>
-            </div>
-            <div className="time-Displayboard-info-countdown"></div>
-         </div>
+      )}
     </div>
-  )
-}
+  );
+};
+
+export const Displayboard = () => {
+  return (
+    <div className="Displayboard-main-container">
+      <div className="Displayboard-info-cont">
+        <div className="series-Displayboard-info-countdown">
+          <div className="series-title-container">
+            <span> Mama Kifitiri Payment Series</span>
+            <span>Product name: Pallet starter</span>
+          </div>
+        </div>
+        <div className="time-Displayboard-info-countdown">
+          <div className="list-paid-series-container">
+            <div className="series-header">
+              <span>Date</span>
+              <span>Time</span>
+              <span>Paid Amout</span>
+            </div>
+            <div  className="series-list-payment-scroll">
+            <div className="series-list-payment">
+              <span>12-12-2025</span>
+              <span>05:06</span>
+              <span>12,000</span>
+            </div>
+            </div>
+          </div>
+          <div className="Paid-reach-suppose">
+            <div>
+            <span>Paiment Required</span>
+            <span>40,000</span>
+            </div>
+            <div>
+              <span>Already Piad</span>
+              <span>12,000</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
