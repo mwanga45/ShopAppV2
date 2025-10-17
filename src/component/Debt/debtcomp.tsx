92c5071be6fdd6dd0a344db8733da62cfd2f773f
mwanga45 <issamwanga02@gmail.com>
2025-10-17 18:18:40 +0300

    more changes

--- src/component/Debt/debtcomp.tsx
+++ src/component/Debt/debtcomp.tsx
@@ -120,12 +120,13 @@ export const Displayboard = () => {
               </div>
             </div>
           </div>
-               <div className="Action-container">
-              <button className="action-btn-debt">Edit</button>
-              <button className="action-btn-debt">Add</button>
-              <button className="action-btn-debt">Send Message</button>
-            </div>
+          <div className="Action-container">
+            <button className="action-btn-debt">Edit</button>
+            <button className="action-btn-debt">Add</button>
+            <button className="action-btn-debt">Send Message</button>
+          </div>
         </div>
+        <div className=""></div>
       </div>
     </div>
   );
