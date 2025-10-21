67763c013228d007630240a372e21fdef7ddca9b
mwanga45 <issamwanga02@gmail.com>
2025-10-21 10:22:50 +0300

    more changes

--- src/central-api/central-api.ts
+++ src/central-api/central-api.ts
@@ -14,3 +14,7 @@ export const  customerInfo = async() =>{
     const  response = await api.get(`debt/${id}`)
     return response
  }
+ export const SendsmsTo = async (data:any) =>{
+   const response = await api.post('sms/send-sms', data)
+   return response
+ }
