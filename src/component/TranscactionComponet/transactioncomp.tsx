933c5c385e701836398856c238f74193129eb805
mwanga45 <issamwanga02@gmail.com>
2025-11-05 14:38:13 +0300

    more changes

--- src/component/TranscactionComponet/transactioncomp.tsx
+++ src/component/TranscactionComponet/transactioncomp.tsx
@@ -2,7 +2,7 @@ import { RiCloseFill } from "react-icons/ri";
 import styles from "./transaction.module.css";
 import AnimatedCard from "../Admincord/animatedcard";
 import type React from "react";
-export const TransactionComp:React.FC = () => {
+export const TransactionComp: React.FC = () => {
   return (
     <div className={styles.transctionmaincontainer}>
       <div className={styles.transcationtopbar}>
@@ -10,11 +10,13 @@ export const TransactionComp:React.FC = () => {
           <RiCloseFill size={30} color="pink" />
         </div>
       </div>
-      <div className={styles.transactiontitle}>
+        <div className={styles.transactiontitle}>
           <span>Transaction Activity</span>
-      </div>
-      <div className={styles.transcionamountcontainer}>
-         <AnimatedCard icon={"symbol"} details={"ON USE"} money={600000}/>
+        </div>
+      <div className={styles.TranscationBody}>
+        <div className={styles.transcionamountcontainer}>
+          <AnimatedCard icon={"symbol"} details={"ON USE"} money={600000} />
+        </div>
       </div>
     </div>
   );
