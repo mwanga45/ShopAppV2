a91b14786b73ec8ed5595bc13c0613adcd73480b
mwanga45 <issamwanga02@gmail.com>
2025-11-05 14:02:59 +0300

    more changes

--- src/component/TranscactionComponet/transactioncomp.tsx
+++ src/component/TranscactionComponet/transactioncomp.tsx
@@ -1,11 +1,16 @@
-import './transaction.modele.css'
-export const TransactionComp = ()=>{
-    return(
-        <div className='transction-main-container'></div>
-    )
-}
-export const TransactionForm =()=>{
-    return(
-        <div></div>
-    )
-}
+import { RiCloseFill } from "react-icons/ri";
+import "./transaction.modele.css";
+export const TransactionComp = () => {
+  return (
+    <div className="transction-main-container">
+    <div className="transcation-top-bar">
+      <div className="icon-close">
+        <RiCloseFill size={30} color="black" />
+      </div>
+      </div>
+    </div>
+  );
+};
+export const TransactionForm = () => {
+  return <div></div>;
+};
