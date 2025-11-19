fddd22efdda565b810b609ac7f7742031ed96ec0
mwanga45 <issamwanga02@gmail.com>
2025-11-19 22:15:10 +0300

    more changes

--- src/layout.ts
+++ src/layout.ts
@@ -1,6 +1,6 @@
 import { api } from "./global.api"
 
 export const CheckCapitalInfo = async()=>{
-    const response = await api.get('checkcapitalinfo')
+    const response = await api.get('management/checkcapitalinfo')
     return response
 }
