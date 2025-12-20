74de9647f51d9878a327223e634b0a3c09ee6ebc
mwanga45 <issamwanga02@gmail.com>
2025-12-20 10:23:07 +0300

    more changes

--- src/component/customer/customerlist.tsx
+++ src/component/customer/customerlist.tsx
@@ -1,23 +1,200 @@
-import styles from './customerlist.module.css'
+import styles from "./customerlist.module.css";
 export const CustomerList = () => {
   return (
     <div>
       <table className={styles.customerlisttable}>
         <thead>
           <tr>
-            <td>S/N</td>
-            <td>CustomerName</td>
-            <td>Location</td>
-            <td>Phone Number</td>
-            <td>Debt-status</td>
-            <td>RegisteredAt</td>
-            <td>View More details</td>
+            <th>S/N</th>
+            <th>CustomerName</th>
+            <th>Location</th>
+            <th>Phone Number</th>
+            <th>Debt-status</th>
+            <th>RegisteredAt</th>
+            <th>View More details</th>
           </tr>
         </thead>
         <tbody>
-            <tr>
-                
-            </tr>
+          <tr>
+            <td>1</td>
+            <td>Issa mwanga</td>
+            <td>Kilimanjaro</td>
+            <td>255744010257</td>
+            <td>None</td>
+            <td>Nov 12, 2025</td>
+            <td>view</td>
+          </tr>
+              <tr>
+            <td>1</td>
+            <td>Issa mwanga</td>
+            <td>Kilimanjaro</td>
+            <td>255744010257</td>
+            <td>None</td>
+            <td>Nov 12, 2025</td>
+            <td>view</td>
+          </tr>
+              <tr>
+            <td>1</td>
+            <td>Issa mwanga</td>
+            <td>Kilimanjaro</td>
+            <td>255744010257</td>
+            <td>None</td>
+            <td>Nov 12, 2025</td>
+            <td>view</td>
+          </tr>
+              <tr>
+            <td>1</td>
+            <td>Issa mwanga</td>
+            <td>Kilimanjaro</td>
+            <td>255744010257</td>
+            <td>None</td>
+            <td>Nov 12, 2025</td>
+            <td>view</td>
+          </tr>
+              <tr>
+            <td>1</td>
+            <td>Issa mwanga</td>
+            <td>Kilimanjaro</td>
+            <td>255744010257</td>
+            <td>None</td>
+            <td>Nov 12, 2025</td>
+            <td>view</td>
+          </tr>
+              <tr>
+            <td>1</td>
+            <td>Issa mwanga</td>
+            <td>Kilimanjaro</td>
+            <td>255744010257</td>
+            <td>None</td>
+            <td>Nov 12, 2025</td>
+            <td>view</td>
+          </tr>
+              <tr>
+            <td>1</td>
+            <td>Issa mwanga</td>
+            <td>Kilimanjaro</td>
+            <td>255744010257</td>
+            <td>None</td>
+            <td>Nov 12, 2025</td>
+            <td>view</td>
+          </tr>
+              <tr>
+            <td>1</td>
+            <td>Issa mwanga</td>
+            <td>Kilimanjaro</td>
+            <td>255744010257</td>
+            <td>None</td>
+            <td>Nov 12, 2025</td>
+            <td>view</td>
+          </tr>
+              <tr>
+            <td>1</td>
+            <td>Issa mwanga</td>
+            <td>Kilimanjaro</td>
+            <td>255744010257</td>
+            <td>None</td>
+            <td>Nov 12, 2025</td>
+            <td>view</td>
+          </tr>
+              <tr>
+            <td>1</td>
+            <td>Issa mwanga</td>
+            <td>Kilimanjaro</td>
+            <td>255744010257</td>
+            <td>None</td>
+            <td>Nov 12, 2025</td>
+            <td>view</td>
+          </tr>
+              <tr>
+            <td>1</td>
+            <td>Issa mwanga</td>
+            <td>Kilimanjaro</td>
+            <td>255744010257</td>
+            <td>None</td>
+            <td>Nov 12, 2025</td>
+            <td>view</td>
+          </tr>
+              <tr>
+            <td>1</td>
+            <td>Issa mwanga</td>
+            <td>Kilimanjaro</td>
+            <td>255744010257</td>
+            <td>None</td>
+            <td>Nov 12, 2025</td>
+            <td>view</td>
+          </tr>
+              <tr>
+            <td>1</td>
+            <td>Issa mwanga</td>
+            <td>Kilimanjaro</td>
+            <td>255744010257</td>
+            <td>None</td>
+            <td>Nov 12, 2025</td>
+            <td>view</td>
+          </tr>
+              <tr>
+            <td>1</td>
+            <td>Issa mwanga</td>
+            <td>Kilimanjaro</td>
+            <td>255744010257</td>
+            <td>None</td>
+            <td>Nov 12, 2025</td>
+            <td>view</td>
+          </tr>
+              <tr>
+            <td>1</td>
+            <td>Issa mwanga</td>
+            <td>Kilimanjaro</td>
+            <td>255744010257</td>
+            <td>None</td>
+            <td>Nov 12, 2025</td>
+            <td>view</td>
+          </tr>
+              <tr>
+            <td>1</td>
+            <td>Issa mwanga</td>
+            <td>Kilimanjaro</td>
+            <td>255744010257</td>
+            <td>None</td>
+            <td>Nov 12, 2025</td>
+            <td>view</td>
+          </tr>
+              <tr>
+            <td>1</td>
+            <td>Issa mwanga</td>
+            <td>Kilimanjaro</td>
+            <td>255744010257</td>
+            <td>None</td>
+            <td>Nov 12, 2025</td>
+            <td>view</td>
+          </tr>
+              <tr>
+            <td>1</td>
+            <td>Issa mwanga</td>
+            <td>Kilimanjaro</td>
+            <td>255744010257</td>
+            <td>None</td>
+            <td>Nov 12, 2025</td>
+            <td>view</td>
+          </tr>
+              <tr>
+            <td>1</td>
+            <td>Issa mwanga</td>
+            <td>Kilimanjaro</td>
+            <td>255744010257</td>
+            <td>None</td>
+            <td>Nov 12, 2025</td>
+            <td>view</td>
+          </tr>
+              <tr>
+            <td>1</td>
+            <td>Issa mwanga</td>
+            <td>Kilimanjaro</td>
+            <td>255744010257</td>
+            <td>None</td>
+            <td>Nov 12, 2025</td>
+            <td>view</td>
+          </tr>
         </tbody>
       </table>
     </div>
